94ed22e9f26e5aeb7b6d4dc34a7f113673d3ce6c
Jack Ma <jm5093@cumc.columbia.edu>
2020-06-22 11:04:16 -0400

    change app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          hello
+          I was changed on the feature branch
         </p>
         <a
           className="App-link"
